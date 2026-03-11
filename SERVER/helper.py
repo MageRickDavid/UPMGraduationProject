55a47f7c260496b972a863594ab3326780002a7a
ricardo <ricardo.ortizpozo@luxpmsoft.com>
2026-03-11 23:45:15 +0900

    the api now hadles getting the stations IDs and there lines based on a substring of their name

---
+++ SERVER/helper.py
@@ -0,0 +1,36 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    helper.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rortiz <rortiz@student.42madrid.com>       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/03/11 22:59:51 by rortiz            #+#    #+#              #
+#    Updated: 2026/03/11 23:32:54 by rortiz           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sqlite3
+
+columns = "name, station_id"
+def find_station(station_name):
+    conn = sqlite3.connect("../databases/cercanias.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        f"SELECT {columns} FROM stations WHERE LOWER(name) LIKE (?)", (f"%{station_name}%",))
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+def get_lines(station_name):
+    station_id = find_station(station_name)
+    station_id = station_id[0][1]
+    conn = sqlite3.connect("../databases/cercanias.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT line_id FROM station_lines WHERE station_id LIKE (?)", (f"%{station_id}%",)
+    )
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
--- SERVER/server.py
+++ SERVER/server.py
@@ -6,25 +6,21 @@
 #    By: rortiz <rortiz@student.42madrid.com>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/03/11 21:59:35 by rortiz            #+#    #+#              #
-#    Updated: 2026/03/11 22:39:37 by rortiz           ###   ########.fr        #
+#    Updated: 2026/03/11 23:16:25 by rortiz           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from fastapi import FastAPI
-import sqlite3
+from helper import *
 
 app = FastAPI()
-
-def find_station(station_name):
-    conn = sqlite3.connect("cercanias.db")
-    cursor = conn.cursor()
-    cursor.execute(
-        "SELECT name, station_id FROM stations WHERE LOWER(name) LIKE (?)", (f"%{station_name}%",))
-    results = cursor.fetchall()
-    conn.close()
-    return results
 
 @app.get("/stations/{name}")
 def get_station(name: str):
     stations = find_station(name)
     return {"query": name, "results": stations}
+
+@app.get("/linesFromStation/{name}")
+def get_lines_from_station_name(name):
+    lines = get_lines(name)
+    return {"query": name, "results": lines}
